a76efbbf7fc8972b90971e3e6703dc4b8f5c5cb0
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-11-14 22:48:40 +0100

    added comparison

--- script.js
+++ script.js
@@ -13,24 +13,17 @@ userBtns.forEach(btn => {
     btn.addEventListener('click', function() {
         // console.log(btn);
 
-        const randomNumber = generateRandomNumber();
         const generatedNumber = generateNumberForBtn(btn);
+        const randomNumber = generateRandomNumber();
         
-        computersChoice.textContent = computerOptions[randomNumber];
-        console.log(randomNumber);
         playersChoice.textContent = computerOptions[generatedNumber];
         console.log(generatedNumber);
+        computersChoice.textContent = computerOptions[randomNumber];
+        console.log(randomNumber);
+        
+        console.log(compareNumbers(generatedNumber, randomNumber))
     });
 });
-
-
-/**
- * Generates random number for random computer chocie
- */
-
-function generateRandomNumber() {
-    return Math.floor(Math.random() * computerOptions.length);
-}
 
 
 /**
@@ -49,11 +42,32 @@ function generateNumberForBtn(btn) {
 
 
 /**
+ * Generates random number for random computer chocie
+ */
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * computerOptions.length);
+}
+
+
+/**
  * Compare computer choice with player choice
  */
 
 function compareNumbers(num1, num2) {
-    if (num1 ) {
-        
+    if (num1 === 0 && num2 === 1) {
+        return 'Player lost! Paper gets cut by scissors!';
+    } else if (num1 === 0 && num2 === 2) {
+        return 'Player won! Paper wraps stone!';
+    } else if (num1 === 1 && num2 === 0) {
+        return 'player won! Scissors cuts paper!'
+    } else if (num1 === 1 && num2 === 2) {
+        return 'Player lost! Scissors become blunt due to stone!';
+    } else if (num1 === 2 && num2 === 0) {
+        return 'Player lost! Stone gets wraped up by paper!';
+    } else if (num1 === 2 && num2 === 1) {
+        return 'Player won! Stone blunts scissors!';
+    } else if (num1 === num2) {
+        return `That's a tie! Restart game!`;
     }
 }
